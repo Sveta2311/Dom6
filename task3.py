1a597803ee0ba0c315b3a8762c8f40d17cbe19ef
Sveta <margaritka2132@mail.ru>
2022-10-12 01:32:46 +0400

    added changes task3

--- task3.py
+++ task3.py
@@ -1,45 +1,31 @@
-# Задайте рандомно список из чисел размером N, где N число с клавиатуры. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+# Напишите программу, удаляющую из текста все слова, содержащие ""абв"".
+# data = open(r"text.txt", "r", encoding="UTF-8")
+# text = data.read()
+# text = text.split(" ")
+# data.close()
+# print(text)
+# new_text = ""
 
-# N = int (input('Bведите число (размер списка) > 0: '))
-# if N > 0:
-#     import random
-#     list = random.sample(range(100), N)
-#     print(f"Сгенерированный список: {list}")
-#     if N == 1:
-#         print("В списке нет элементов, стоящих на нечетных позициях!")
-#     else:
-#         print("Элементы списка, стоящие на нечетной позиции: ")
-#     for i in range(1, len(list), 2):
-#             if i == N-1 or i == N-2:
-#                 print(list[i])
-#             else:
-#                 print(list[i], end=", ")
-#     else:
-#         print(f"Сумма элементов списка, стоящих на нечетной позиции: {sum(list[1::2])}") 
-# else:
-#     print("Ошибка ввода!")
+# for i in range(len(text)-1):
+#     if (text[i].lower().find("абв") == -1):
+#             new_text+= text[i]+" "
 
-# filter
+# data = open (r"text1.txt", "w", encoding="UTF-8")
+# data.write(new_text)
+# data.close()
 
-import random
-n = int (input('Bведите число (размер списка) > 0: '))
-if n > 0:
-    list1 = random.sample(range(100), n)
-    print(f"Сгенерированный список: {list1}")
-    if n == 1:
-        print("В списке нет элементов, стоящих на нечетных позициях!")
-        exit()
-    else:
-        def filter_odd_indices(a):
-            if list1.index(a)%2 == 1:
-                return True
-            else:
-                return False
+# enumerate
 
-    out_filter = list(filter(filter_odd_indices, list1))
+with open(r"text.txt", "r", encoding="UTF-8") as data:
+    text = data.read()
+text = text.lower().split(" ")
+new_txt = ''
 
-    print("Элементы списка, стоящие на нечетной позиции:",out_filter)
-    print("Сумма элементов списка, стоящих на нечетной позиции:",sum(out_filter))
-
-else:
-    print("Ошибка ввода!")
+for i, el in enumerate(text):
+    if "абв" in el:
+            text.pop(i)
+for el in text:
+    new_txt += el + ' '
+        
+with open (r"text1.txt", "w", encoding="UTF-8") as data:
+    data.write(new_txt)
